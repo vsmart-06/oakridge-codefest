d088426f831736e2d6fe941e6797b0c7cff9ca9d
vsmart-06 <srivishnuvusirikala@gmail.com>
2023-01-22 04:01:03 +0530

    Added warning

--- recommend.py
+++ recommend.py
@@ -15,15 +15,17 @@ class Recommendation:
         self.username = username
         self.root = tk.Tk()
         self.root.title("Recommendations")
-        self.root.geometry("350x400")
+        self.root.geometry("600x400")
         self.root.tk.call("source", "./oakridge-codefest/forest-dark.tcl")
         ttk.Style().theme_use("forest-dark")
 
         self.frame = ttk.Frame(self.root)
         self.frame.grid(row = 0, column = 1, padx = 10)
 
+        self.warning = ttk.Label(self.frame, text = "Need some recommendations on events you can possibly hold? \nTry clicking the AI recommendation button. \nBeware it is still experimental and will take some time!!")
+        self.warning.grid(row = 0, column = 0, pady = 10)
         button = ttk.Button(self.frame, text = "Get Recommendations", style = "Accent.TButton", command = self.giveReccomendation)
-        button.grid(row = 0, column = 1)
+        button.grid(row = 1, column = 0)
 
         self.root.update()
         sidebar = Sidebar(self.root, self.username)
@@ -37,7 +39,7 @@ class Recommendation:
         except:
             pass
         self.myLabel = ttk.Label(self.frame, text = text)
-        self.myLabel.grid(row = 1, column = 1)
+        self.myLabel.grid(row = 2, column = 0, pady = 10)
 
     def giverec(self):
         openai.api_key = API_KEY
@@ -46,7 +48,7 @@ class Recommendation:
         dataset = [file1, file2]
 
         model = "davinci"
-        prompt = "Fine-tune the model to give similar results when asked for 1 reccomendation on possible events to be conducted to be more environmentally conscious: " + " | " .join(dataset)   
+        prompt = "Fine-tune the model to give similar results when asked for only 1 reccomendation on possible events to be conducted to be more environmentally conscious: " + " | " .join(dataset)   
 
 
         response = openai.Completion.create(
