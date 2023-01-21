e5418a8d92fd2454ab17285bd0edc1bd5c832f8c
vsmart-06 <srivishnuvusirikala@gmail.com>
2023-01-21 14:12:27 +0530

    Fixed some sidebar errors

--- event_page.py
+++ event_page.py
@@ -11,25 +11,28 @@ class Events:
         self.window.tk.call("source", "./oakridge-codefest/forest-dark.tcl")
         ttk.Style().theme_use("forest-dark")
 
-        sidebar = Sidebar(self.window)
+        self.main_frame = ttk.Frame(self.window)
+        self.main_frame.grid(row = 0, column = 1)
 
-        self.title_label = ttk.Label(self.window, text = "Title:")
-        self.title_label.grid(row = 0, column = 1, padx = 10, pady = 10)
-        self.title_entry = ttk.Entry(self.window, width = 50)
-        self.title_entry.grid(row = 0, column = 2, padx = 10, pady = 10)
+        self.title_label = ttk.Label(self.main_frame, text = "Title:")
+        self.title_label.grid(row = 0, column = 0, padx = 10, pady = 10)
+        self.title_entry = ttk.Entry(self.main_frame, width = 50)
+        self.title_entry.grid(row = 0, column = 1, padx = 10, pady = 10)
 
-        self.description_label = ttk.Label(self.window, text = "Description:")
-        self.description_label.grid(row = 1, column = 1, padx = 10, pady = (0, 10), sticky = "n")
-        self.description_frame = ttk.Frame(self.window, style = "Card", borderwidth = 1)
-        self.description_frame.grid(row = 1, column = 2, padx = 10, pady = (0, 10))
+        self.description_label = ttk.Label(self.main_frame, text = "Description:")
+        self.description_label.grid(row = 1, column = 0, padx = 10, pady = (0, 10), sticky = "n")
+        self.description_frame = ttk.Frame(self.main_frame, style = "Card", borderwidth = 1)
+        self.description_frame.grid(row = 1, column = 1, padx = 10, pady = (0, 10))
         self.description_entry = tk.Text(self.description_frame, width = 50, height = 20, borderwidth = 0)
         self.description_entry.grid(row = 0, column = 0, padx = 5, pady = 5)
 
 
-        cal_label = ttk.Label(self.window, text = "Choose the date for the event:")
-        cal_label.grid(row = 2, column = 1, padx = 10, pady = (0, 10))
-        cal = DateEntry(self.window)
-        cal.set_date(datetime.date.today())
-        cal.grid(row = 2, column = 2, padx = 10, pady = (0, 10), sticky = "w")
+        self.cal_label = ttk.Label(self.main_frame, text = "Choose the date for the event:")
+        self.cal_label.grid(row = 2, column = 0, padx = 10, pady = (0, 10))
+        self.cal = DateEntry(self.main_frame)
+        self.cal.set_date(datetime.date.today())
+        self.cal.grid(row = 2, column = 1, padx = 10, pady = (0, 10), sticky = "w")
+
+        self.sidebar = Sidebar(self.window)
 
         self.window.mainloop()
